e19135437ede838c982441b6e65af5f0349c4819
born2net@gmail.com <connectconnect123>
2019-06-07 13:31:55 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'studio-ng';
+    title = 'studio-ng';
+
+    constructor() {
+        const abc = 22;
+        console.log(abc);
+    }
 }
