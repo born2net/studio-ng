9bdeccb04ebed1185e5a6585ffa171e92cc2de5d
born2net@gmail.com <connectconnect123>
2019-06-10 12:20:58 -0700

    updating ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
+import {StoreModule} from '@ngrx/store';
+import {appReducers} from './store/reducers/app.reducers';
+import {ConfigEffects} from './store/effects/config.effects';
+import {UserEffects} from './store/effects/user.effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+// import { AppEffects } from './app.effects';
+
+
 
 @NgModule({
     declarations: [
@@ -15,7 +23,11 @@ import { AppEffects } from './app.effects';
         BrowserModule,
         AppRoutingModule,
         NgbModule,
-        EffectsModule.forRoot([AppEffects])
+        StoreModule.forRoot(appReducers),
+        EffectsModule.forRoot([UserEffects, ConfigEffects]),
+        // StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
+        !environment.production ? StoreDevtoolsModule.instrument() : [],
+
     ],
     providers: [],
     bootstrap: [AppComponent]
