f846c9f5a55937c268bb0b11e8c69101f7156975
born2net@gmail.com <connectconnect123>
2019-06-11 11:25:53 -0700

    added FA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+import {faStar as farStar1} from '@fortawesome/free-regular-svg-icons';
+import {faStar as fasStar2} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
     selector: 'app-root',
@@ -7,6 +11,10 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
     title = 'studio-ng';
+    public faCoffee = faCoffee;
+    public farStar1 = farStar1;
+    public fasStar2 = fasStar2;
+    public magicLevel;
 
     constructor() {
         const abc = 22;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { EffectsModule } from '@ngrx/effects';
+import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
 import {appReducers} from './store/reducers/app.reducers';
 import {ConfigEffects} from './store/effects/config.effects';
@@ -13,6 +13,8 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
 import {UserService} from './services/user.service';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
 // import { AppEffects } from './app.effects';
 
 // ref: https://medium.com/frontend-fun/angular-ngrx-a-clean-and-clear-introduction-4ed61c89c1fc
@@ -26,9 +28,10 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
         AppRoutingModule,
         HttpClientModule,
         NgbModule,
+        FontAwesomeModule,
         StoreModule.forRoot(appReducers),
         EffectsModule.forRoot([UserEffects, ConfigEffects]),
-        StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
+        StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
         !environment.production ? StoreDevtoolsModule.instrument() : [],
 
     ],
